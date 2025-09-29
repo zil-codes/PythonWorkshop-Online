23f4928173710a513ac2117047e9b5d607a746d0
zill <zill.sqa@gmail.com>
2025-09-29 09:49:16 -0700

    Add all project items

---
+++ Datatype.py
@@ -0,0 +1,27 @@
+# Numbers → int, float, complex
+
+# Sequences → str, list, tuple
+
+# Sets → set, frozenset
+
+# Mapping → dict
+
+# Boolean → bool
+
+# Binary → bytes, bytearray, memoryview
+
+# Special → None
+
+# 1. Numeric Types
+#
+# Used to store numbers.
+#
+# int → Integer numbers (positive, negative, zero)
+#
+# float → Decimal (floating-point) numbers
+#
+# complex → Numbers with real and imaginary parts
+
+a = 10  # int
+b = 3.14  # float
+c = 2 + 5j  # complex
